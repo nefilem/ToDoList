96a64f7e9d06441fc216a1779cb5d43b42a90e43
Keiron Lee Goodwin <41470136+nefilem@users.noreply.github.com>
2022-03-22 11:46:18 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import './App.css';
+import View from './view'
+
+function App(){
+
+    return (
+        <Container>
+          <View />
+        </Container>
+    );
+
+}
+export default App;
